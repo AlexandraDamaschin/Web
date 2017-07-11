ae010e4a90c3cb5e5dea65450a4bf68a6a9a67f3
Asus-PC\Asus <damaschin.alexandra.maria@gmail.com>
2017-07-11 21:14:46 +0300

    added xml and json

---
+++ BlogEntry.php
@@ -0,0 +1,24 @@
+<div class="w3-card-4 w3-margin w3-white">
+    <img src="img/desktop.png" alt="desktop" style="width:100%">
+    <div class="w3-container">
+        <h3><b>{$title}</b></h3>
+        <h5>{$description}, <span class="w3-opacity">{$date}</span></h5>
+    </div>
+
+    <div class="w3-container">
+        <p>{$content}</p>
+        <div class="w3-row">
+            <div class="w3-col m8 s12">
+                <p>
+                    <button class="w3-button w3-padding-large w3-white w3-border"><a
+                            href="PageHTML.html"><b>READ MORE »</b></a></button>
+                </p>
+            </div>
+            <div class="w3-col m4 w3-hide-small">
+                <p><span class="w3-padding-large w3-right"><a href="PageHTML.html"><b>Comments</b> </a><span
+                            class="w3-tag">0</span></span></p>
+            </div>
+        </div>
+    </div>
+</div>
+<hr>
