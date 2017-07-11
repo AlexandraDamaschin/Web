9b9ac1831890d5379de67c525d0bb3a55d6f77a8
catalinaavram <maria.avram@ulbsibiu.ro>
2017-07-11 14:17:52 +0300

    Add files via upload
    bla bla

--- BlogPage2.php
+++ BlogPage2.php
@@ -25,9 +25,9 @@
         <div class="w3-col l8 s12">
             <!-- Blog entry -->
             <div class="w3-card-4 w3-margin w3-white">
-                <img src="img/desktop.png" alt="desktop" style="width:100%">
+                <img src="img/json.png" alt="desktop" style="width:100%">
                 <div class="w3-container">
-                    <h3><b>HTML</b></h3>
+                    <h3><b>JSON</b></h3>
                     <h5>Title description, <span class="w3-opacity">April 7, 2014</span></h5>
                 </div>
 
@@ -36,10 +36,10 @@
                         tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.</p>
                     <div class="w3-row">
                         <div class="w3-col m8 s12">
-                            <p><button class="w3-button w3-padding-large w3-white w3-border"><a href="PageHTML.html"><b>READ MORE »</b></a></button></p>
+                            <p><button class="w3-button w3-padding-large w3-white w3-border"><a href="PageJSON.html"><b>READ MORE »</b></a></button></p>
                         </div>
                         <div class="w3-col m4 w3-hide-small">
-                            <p><span class="w3-padding-large w3-right"><a href="PageHTML.html"><b>Comments</b> </a><span class="w3-tag">0</span></span></p>
+                            <p><span class="w3-padding-large w3-right"><a href="PageJSON.html"><b>Comments</b></a> <span class="w3-tag">0</span></span></p>
                         </div>
                     </div>
                 </div>
@@ -48,9 +48,9 @@
 
             <!-- Blog entry -->
             <div class="w3-card-4 w3-margin w3-white">
-                <img src="img/xml.jpg" alt="Xml" style="width:100%">
+                <img src="img/blue.jpg" alt="blue" style="width:100%">
                 <div class="w3-container">
-                    <h3><b>XML</b></h3>
+                    <h3><b>CSS</b></h3>
                     <h5>Title description, <span class="w3-opacity">April 2, 2014</span></h5>
                 </div>
 
@@ -59,10 +59,32 @@
                         tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.</p>
                     <div class="w3-row">
                         <div class="w3-col m8 s12">
-                            <p><button class="w3-button w3-padding-large w3-white w3-border"><a href="PageXML.html"><b>READ MORE »</b></a></button></p>
+                            <p><button class="w3-button w3-padding-large w3-white w3-border"><a href="PageCSS.html"><b>READ MORE »</b></a></button></p>
                         </div>
                         <div class="w3-col m4 w3-hide-small">
-                            <p><span class="w3-padding-large w3-right"><a href="PageXML.html"><b>Comments</b></a> <span class="w3-badge">2</span></span></p>
+                            <p><span class="w3-padding-large w3-right"><a href="PageCSS.html"><b>Comments</b> </a><span class="w3-badge">2</span></span></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <!-- Blog entry -->
+            <div class="w3-card-4 w3-margin w3-white">
+                <img src="img/sass.png" alt="Xml" style="width:100%">
+                <div class="w3-container">
+                    <h3><b>SASS</b></h3>
+                    <h5>Title description, <span class="w3-opacity">April 2, 2014</span></h5>
+                </div>
+
+                <div class="w3-container">
+                    <p>Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl. Sed mattis nunc id lorem euismod placerat. Vivamus porttitor magna enim, ac accumsan tortor cursus at. Phasellus sed ultricies mi non congue ullam corper. Praesent tincidunt sed
+                        tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.</p>
+                    <div class="w3-row">
+                        <div class="w3-col m8 s12">
+                            <p><button class="w3-button w3-padding-large w3-white w3-border"><a href="PageSASS.html"><b>READ MORE »</b></a></button></p>
+                        </div>
+                        <div class="w3-col m4 w3-hide-small">
+                            <p><span class="w3-padding-large w3-right"><a href="PageSASS.html"><b>Comments</b> </a><span class="w3-badge">2</span></span></p>
                         </div>
                     </div>
                 </div>
@@ -86,7 +108,7 @@
                 </div>
             </div><hr>
 
-            <!-- Posts -->
+           <!-- Posts -->
             <div class="w3-card-2 w3-margin">
                 <div class="w3-container w3-padding">
                     <h4>Popular Posts</h4>
@@ -143,8 +165,7 @@
     <!-- END w3-content -->
 </div>
 <footer class="w3-container w3-dark-grey w3-padding-32 w3-margin-top">
-    <button class="w3-button w3-black w3-disabled w3-padding-large w3-margin-bottom">Previous</button>
-    <button class="w3-button w3-black w3-padding-large w3-margin-bottom"><a href="BlogPage2.html">Next »</a></button>
+    <button class="w3-button w3-black w3-padding-large w3-margin-bottom"><a href="Blog.html">Previous</a></button>
     <p>&copy; 2017 by Alexandra Damaschin & Catalina Avram</p>
 </footer>
 </body>
--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
-<!-- Footer -->
+﻿<!-- Footer -->
 <footer class="w3-container w3-dark-grey w3-padding-32 w3-margin-top">
     <button class="w3-button w3-black w3-disabled w3-padding-large w3-margin-bottom">Previous</button>
     <button class="w3-button w3-black w3-padding-large w3-margin-bottom">Next »</button>
-    <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">@ Alexandra </a></p>
+    <p>&copy; 2017 by Alexandra Damaschin & Catalina Avram</p>
 </footer>
