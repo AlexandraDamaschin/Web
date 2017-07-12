50e04ce76ea1e708efaaf0f3f83b82905bc3141b
Asus-PC\Asus <damaschin.alexandra.maria@gmail.com>
2017-07-12 11:56:18 +0300

    Added Footer.php

---
+++ Footer.php
@@ -0,0 +1,5 @@
+<footer class="w3-container w3-dark-grey w3-padding-32 w3-margin-top">
+    <button class="w3-button w3-black w3-disabled w3-padding-large w3-margin-bottom">Previous</button>
+    <button class="w3-button w3-black w3-padding-large w3-margin-bottom">Next »</button>
+    <p>&copy; 2017 by Alexandra Damaschin & Catalina Avram</p>
+</footer>
